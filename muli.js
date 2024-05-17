bdf9f2d91a061181157e7cce599a2a630b6045cf
Fareed Bakhsh <fareedbakhsh373@gmail.com>
2024-05-17 19:05:39 +0500

    first commit

---
+++ muli.js
@@ -0,0 +1,15 @@
+function showImage1(imageUrl) {
+  document.getElementById('mainImage1').src = imageUrl;
+}
+
+function showImage2(imageUrl) {
+  document.getElementById('mainImage2').src = imageUrl;
+}
+
+function showImage3(imageUrl) {
+  document.getElementById('mainImage3').src = imageUrl;
+}
+
+function showImage4(imageUrl) {
+  document.getElementById('mainImage4').src = imageUrl;
+}
